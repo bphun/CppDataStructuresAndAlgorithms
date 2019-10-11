b5036d53900c8ba7343dbe677178428f2d7f118c
Brandon Phan <brandon.phan.73@gmail.com>
2019-10-10 22:27:05 -0700

    Implement jump search

---
+++ algorithms/Search/JumpSearch/JumpSearch.h
@@ -0,0 +1,107 @@
+#ifndef JumpSearch_h
+
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+template <typename T>
+bool jumpSearch(T arr[], const T &searchVal, const size_t &arrSize)
+{
+    if (arrSize == 0)
+        return false;
+
+    size_t stepSize = sqrt(arrSize);
+    size_t prev = 0;
+
+    while (arr[min(stepSize, arrSize) - 1] < searchVal)
+    {
+        prev = stepSize;
+        stepSize += sqrt(arrSize);
+
+        if (prev >= arrSize)
+            return false;
+    }
+
+    while (arr[prev] < searchVal)
+    {
+        prev++;
+
+        if (prev == min(stepSize, arrSize))
+            return false;
+    }
+
+    if (arr[prev] == searchVal)
+        return true;
+
+    return false;
+}
+
+template <typename T>
+bool jumpSearch(T **arr, const T &searchVal, const size_t &arrSize)
+{
+    if (arrSize == 0)
+        return false;
+
+    size_t stepSize = sqrt(arrSize);
+    int prev = 0;
+
+    while (*arr[min(stepSize, arrSize) - 1] < searchVal)
+    {
+        prev = stepSize;
+        stepSize += sqrt(arrSize);
+
+        if (prev >= arrSize)
+            return false;
+    }
+
+    while (*arr[prev] < searchVal)
+    {
+        prev++;
+
+        if (prev == min(stepSize, arrSize))
+            return false;
+    }
+
+    if (*arr[prev] == searchVal)
+        return true;
+
+    return false;
+}
+
+template <typename T>
+bool jumpSearch(vector<T> arr, T searchVal)
+{
+    if (arr.size() == 0)
+        return false;
+
+    size_t arrSize = arr.size();
+    size_t stepSize = sqrt(arrSize);
+    int prev = 0;
+
+    while (arr[min(stepSize, arrSize) - 1] < searchVal)
+    {
+        prev = stepSize;
+        stepSize += sqrt(arrSize);
+
+        if (prev >= arrSize)
+            return false;
+    }
+
+    while (arr[prev] < searchVal)
+    {
+        prev++;
+
+        if (prev == min(stepSize, arrSize))
+        {
+            return false;
+        }
+    }
+
+    if (arr[prev] == searchVal)
+        return true;
+
+    return false;
+}
+
+#endif
---
+++ algorithms/Search/JumpSearch/test/JumpSearchTest.cpp
@@ -0,0 +1,78 @@
+#include "gtest/gtest.h"
+#include "../JumpSearch.h"
+
+#define ARRAY_SIZE 1000
+
+int main(int argc, char **argv)
+{
+    srand(time(nullptr));
+
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(JumpSearch, Array_SearchValIsMemberOfArray)
+{
+    int array[ARRAY_SIZE];
+    int randIndex = rand() % ARRAY_SIZE;
+
+    for (size_t i = 0; i < ARRAY_SIZE - 1; i++)
+        array[i] = i;
+
+    EXPECT_TRUE(jumpSearch(array, array[randIndex], ARRAY_SIZE));
+}
+
+TEST(JumpSearch, Array_SearchValIsNotMemberOfArray)
+{
+    int array[ARRAY_SIZE];
+    int searchVal = ARRAY_SIZE + 1;
+
+    for (size_t i = 0; i < ARRAY_SIZE; i++)
+        array[i] = i;
+
+    EXPECT_FALSE(jumpSearch(array, searchVal, ARRAY_SIZE));
+}
+
+TEST(JumpSearch, PointerArray_SearchValIsMemberOfArray)
+{
+    int *array[ARRAY_SIZE] = {nullptr};
+    int randIndex = rand() % ARRAY_SIZE;
+
+    for (size_t i = 0; i < ARRAY_SIZE - 1; i++)
+        array[i] = new int(i);
+
+    EXPECT_TRUE(jumpSearch(array, array[randIndex], ARRAY_SIZE));
+}
+
+TEST(JumpSearch, PointerArray_SearchValIsNotMemberOfArray)
+{
+    int *array[ARRAY_SIZE] = {nullptr};
+    int searchVal = ARRAY_SIZE + 1;
+
+    for (size_t i = 0; i < ARRAY_SIZE; i++)
+        array[i] = new int(i);
+
+    EXPECT_FALSE(jumpSearch(array, searchVal, ARRAY_SIZE));
+}
+
+TEST(JumpSearch, Vector_SearchValIsMemberOfArray)
+{
+    vector<int> array(ARRAY_SIZE);
+    int randIndex = rand() % ARRAY_SIZE;
+
+    for (size_t i = 0; i < ARRAY_SIZE - 1; i++)
+        array[i] = i;
+
+    EXPECT_TRUE(jumpSearch(array, array[randIndex]));
+}
+
+TEST(JumpSearch, Vector_SearchValIsNotMemberOfArray)
+{
+    vector<int> array(ARRAY_SIZE);
+    int searchVal = ARRAY_SIZE + 1;
+
+    for (size_t i = 0; i < ARRAY_SIZE; i++)
+        array[i] = i;
+
+    EXPECT_FALSE(jumpSearch(array, searchVal));
+}
