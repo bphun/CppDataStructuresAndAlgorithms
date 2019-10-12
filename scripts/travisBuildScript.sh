0ed923055b9251b92a15308e077ec43e29fcc11b
Brandon Phan <brandon.phan.73@gmail.com>
2019-10-11 21:37:15 -0700

    Test commit

--- scripts/travisBuildScript.sh
+++ scripts/travisBuildScript.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-
+currDir=$PWD
 CHANGED_FILES=$(git diff --name-only $TRAVIS_COMMIT_RANGE)
 echo $CHANGED_FILES
-for file in $CHANGED_FILES; do
-    dir=$(dirname $file);
-    echo $dir;
-done
 
-if [ -f "$dir/CMakeLists.txt" ]; then    
-    ./scripts/buildProjectAtDir.sh $dir;
-elif [ -f "$dir/../CMakeLists.txt" ]; then    
-    ./scripts/buildProjectAtDir.sh $dir;
-fi
+for file in $CHANGED_FILES; do
+    dir=$(dirname $file)
+    echo $dir
+
+    cd $dir
+
+    if [ -f "$CMakeLists.txt" ]; then    
+        ./scripts/buildProjectAtDir.sh $dir;
+    elif [ -f "../CMakeLists.txt" ]; then    
+        ./scripts/buildProjectAtDir.sh $dir;
+    fi
+
+    cd $currDir
+done
 
