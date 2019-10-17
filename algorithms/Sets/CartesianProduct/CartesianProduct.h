ac88078899768857b34c5be1754df627f2404632
Brandon Phan <brandon.phan.73@gmail.com>
2019-10-17 10:26:11 -0700

    Implement cartesian product

---
+++ algorithms/Sets/CartesianProduct/CartesianProduct.h
@@ -0,0 +1,21 @@
+#ifndef CartesianProduct_h
+#define CartesianProduct_h
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+template <typename T>
+void cartesianProduct(vector<T> arrA, vector<T> arrB, vector<vector<T>>& result)
+{
+
+    for (int i = 0; i < arrA.size(); i++) {
+        vector<T> cartesianPair(2);
+        for (int j = 0; j < arrB.size(); j++) {
+            cartesianPair = { arrA[i], arrB[j] };
+            result.push_back(cartesianPair);
+        }
+    }
+}
+
+#endif
